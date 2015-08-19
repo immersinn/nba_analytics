eba8d26c4145e714bde870fc46a1b04212e2b149
immersinn <immersinn.gmail.com>
2015-08-18 18:28:25 -0600

    updated nba game query files

--- task_runmes/queryGameBoxWriteFiles.py
+++ task_runmes/queryGameBoxWriteFiles.py
@@ -45,9 +45,86 @@ def createGameBoxInfoFile():
     print('Game Box Info File written to %s' % out_file)
 
 
+def selectiveQueryandWrite():
+
+    out_file = '/home/immersinn/Data/DataDumps/NBA/MultiSeasonGameBoxInfo.csv'
+
+    seasons = ['2008-2009 Regular', '2008-2009 Playoffs',
+               '2009-2010 Regular', '2009-2010 Playoffs',
+               '2010-2011 Regular', '2010-2011 Playoffs',
+               '2011-2012 Regular', '2011-2012 Playoffs',]
+    
+    # Get game ids of interest
+    db_conn = connectMon.MongoConn({'db_name':'NBAData',
+                                    'coll_name':'Games'})
+    teams_cursor = db_conn.query(spec = {'season' : {'$in' : seasons}},
+                                 fields = {'game_stats.teams' : 1,
+                                           'game_id' : 1,
+                                           'season' : 1,
+                                           '_id' : 0},
+                                 limit = 0)
+    game_ids = [entry['game_id'] for entry in teams_cursor if entry['game_stats']]
+
+    db_conn = connectMon.MongoConn({'db_name':'NBAData',
+                                    'coll_name':'Games'})
+
+
+    # Query game data for game ids of interest
+    with open(out_file, 'w') as f1:
+        header = 'GameID,Team'
+        atrs = []
+
+        for gid in game_ids:
+            teams_cursor = db_conn.query(spec = {'game_id' : gid},
+                                         fields = {'game_stats.stats' : 1,
+                                                   'game_id' : 1,
+                                                   '_id' : 0},
+                                         limit = 0,
+                                         verbose = False)
+            if teams_cursor.count(with_limit_and_skip=True) == 1:
+                game = teams_cursor.next()
+                if not atrs:
+                    atrs = game['game_stats']['stats']['home'].keys()
+                    mas = []
+                    for i,a in enumerate(atrs):
+                        if a.endswith('M-A'):
+                            header += ',' + a[:-3] + '-M'
+                            header += ',' + a[:-3] + '-A'
+                            mas.append(i)
+                        else:
+                            header += ',' + a
+                    header += '\n'
+                    f1.write(header)
+
+                for t in ['home', 'away']:
+                    data_line = [game['game_id']]
+                    data_line.append(t)
+                    for i,val in enumerate(game['game_stats']['stats'][t].values()):
+                        if i in mas:
+                            data_line.extend(val.split('-'))
+                        else:
+                            data_line.append(val)
+                    data_line = ','.join(data_line)
+                    data_line += '\n'
+                    f1.write(data_line)
+                    
+            else:
+                for t in ['home', 'away']:
+                    data_line = [gid]
+                    data_line.append(t)
+                    data_line.extend([',' for _ in range(len(header) - 2)])
+                    data_line = ''.join(data_line)
+                    data_line += '\n'
+                    f1.write(data_line)
+
+    print('Game Box Info File written to %s' % out_file)
+      
+        
+
+
 def main():
     createGameBoxInfoFile()
 
 
 if __name__=="__main__":
-    main()
+    selectiveQueryandWrite()
--- task_runmes/queryGameTeamsAndShotAndWriteFiles.py
+++ task_runmes/queryGameTeamsAndShotAndWriteFiles.py
@@ -1,7 +1,7 @@
 
 import sys
 
-sys.path.append('/Users/immersinn/Gits/')
+#sys.path.append('/Users/immersinn/Gits/')
 from  dbinterface_python.dbconns import connectMon
 
 
@@ -54,10 +54,79 @@ def createShotInfoFile():
     print('Shots File written to %s' % out_file)
 
 
+def selectiveQuery():
+
+    game_out_file = '/home/immersinn/Data/DataDumps/NBA/MultiSeasonGameTeamInfo.csv'
+    shot_out_file = '/home/immersinn/Data/DataDumps/NBA/MultiSeasonShotsInfo.csv'
+    
+
+    seasons = ['2008-2009 Regular', '2008-2009 Playoffs',
+               '2009-2010 Regular', '2009-2010 Playoffs',
+               '2010-2011 Regular', '2010-2011 Playoffs',
+               '2011-2012 Regular', '2011-2012 Playoffs',]
+    game_ids = []
+
+
+    # Query for games in seasons specified and write game info from
+    # these games to a file
+    db_conn = connectMon.MongoConn({'db_name':'NBAData',
+                                    'coll_name':'Games'})
+    
+    teams_cursor = db_conn.query(spec = {'season' : {'$in' : seasons}},
+                                 fields = {'game_stats.teams' : 1,
+                                           'game_id' : 1,
+                                           'season' : 1,
+                                           '_id' : 0},
+                                 limit = 0)
+
+    with open(game_out_file, 'w') as f1:
+        header = 'game_id,season,home,away\n'
+        f1.write(header)
+        for entry in teams_cursor:
+            if entry['game_stats']:
+                game_ids.append(entry['game_id'])
+                data_line = [entry['game_id'],
+                             entry['season'],
+                             entry['game_stats']['teams']['home'],
+                             entry['game_stats']['teams']['away']]
+                data_line = ','.join(data_line)
+                data_line += '\n'
+                f1.write(data_line)
+    print('Game-Team File written to %s' % game_out_file)
+
+
+    # Use 'game_ids' from above to query ShotInfo corresponding to the
+    # games in question
+    shots = connectMon.MongoConn({'db_name':'NBAData',
+                                  'coll_name':'Shots'})
+
+    names = ['game_id', 'pts', 'made', 't', 'y', 'x', 'shot_id']
+    header = ','.join(names)
+    
+    with open(shot_out_file, 'w') as f1:
+        f1.write(header)
+        for gid in game_ids:
+            shots_cursor = shots.query(spec = {'game_id' : gid},
+                                       fields = {'_id' : 0,
+                                                 'game_id' : 1,
+                                                 'pts' : 1,
+                                                 'made' : 1,
+                                                 't' : 1,
+                                                 'y' : 1, 'x' : 1,
+                                                 'shot_id' : 1},
+                                       limit = 0)
+            for sd in shots_cursor:
+                data_line = ','.join([str(sd[n]) for n in names])
+                data_line += '\n'
+                f1.write(data_line)
+    print('Shots File written to %s' % shot_out_file)
+                   
+
+
 def main():
     createGameTeamInfoFile()
     createShotInfoFile()
 
 
 if __name__=="__main__":
-    main()
+    selectiveQuery()
